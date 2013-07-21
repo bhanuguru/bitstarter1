23f8a3f38b3acb5bdb2b03af17e6e5af24023ca1
bhanuguru <bhanu.kovuri@gmail.com>
2013-07-21 03:31:30 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
+var buffer = new Buffer(30);
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('./index.html', function(err, data) {
+     if(err) {
+         throw err;
+      } 
+      buffer.write(data);
+  }   
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
