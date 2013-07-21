d638081ce7957c260620727854fa9c85c88fc19c
bhanuguru <bhanu.kovuri@gmail.com>
2013-07-21 04:16:58 +0000

    Update web.js

--- web.js
+++ web.js
@@ -5,11 +5,7 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('./index.html', function(err, data) {
-     if(err) {
-         throw err;
-      }
-  }   
+  var data = fs.readFileSync('index.html', 'utf8');
   response.send(data);
 });
 
